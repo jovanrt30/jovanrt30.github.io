88b38c1a2a00392eb7aa30d733dc47696afe99df
jovanrt30 <jovanrt30@gmail.com>
2021-01-10 17:02:28 +0700

    add vue

---
+++ script.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '.home-title',
+    data: {
+        message: 'Hello, World'
+    }
+}
+)
+
+var app2 = new Vue({
+    el: '.home-subtitle',
+    data: {
+        message: 'I\'m hosted with GitHub Pages.'
+    }
+})
