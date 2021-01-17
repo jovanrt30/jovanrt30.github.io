e6aa80a83f14cc9711f3666be0dbab5993043f08
jovanrt30 <jovanrt30@gmail.com>
2021-01-17 21:41:57 +0700

    add button

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 var app = new Vue({
-    el: '.home-title',
-    data: {
-        message: 'Hello, World'
-    }
-}
-)
-
-var app2 = new Vue({
-    el: '.home-subtitle',
-    data: {
-        message: 'I\'m hosted with GitHub Pages.'
+    el: '.home',
+    data:{
+        show: true,
+        title: 'Hello, Worlds',
+        subtitle: 'I\'m hosted with GitHub Pages.'
     }
 })
